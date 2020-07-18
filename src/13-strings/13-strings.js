0f3ac82e88320df623418b1d4cc9132b23f88368
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-18 12:21:10 -0700

    refactor string challenge 2

--- src/13-strings/13-strings.js
+++ src/13-strings/13-strings.js
@@ -38,6 +38,20 @@ Write a function named findHappiness that takes in an array of strings and retur
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this is great :)', ':)))))']
 ------------------------------------------------------------------------------------------------ */
 
+export const findHappiness = (arr) => {
+    return arr.filter(string => string.includes(':)'));
+
+    // const happiness = arr.filter(string => {
+    //     if(string.includes(':)')) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
+    // return happiness;
+};
+
+
 // export const findHappiness = (arr) => {
 //     const happiness = arr.forEach(string => {
 //         if(string.includes(':)')) {
@@ -62,16 +76,6 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 //     return happiness;
 // };
 
-export const findHappiness = (arr) => {
-    const happiness = arr.filter(string => {
-        if(string.includes(':)')) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-    return happiness;
-};
 
 
 /* ------------------------------------------------------------------------------------------------
